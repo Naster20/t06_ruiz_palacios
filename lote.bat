b988ac596cb4b7e0cf2cd545e92064633d677d08
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-06 16:01:50 -0500

    Condicional simple 03

--- lote.bat
+++ lote.bat
@@ -3,5 +3,9 @@ python ruiz/simple01.py "Ruiz"
 python ruiz/simple01.py "Pedro"
 
 python ruiz/simple02.py 10 20
-python ruiz/simple02.py 15 25
+python ruiz/simple02.py 15 20
 python ruiz/simple02.py 9.5 50
+
+python ruiz/simple03.py "Ruiz" 15 16 17
+python ruiz/simple03.py "Arevalo" 15 12 11
+python ruiz/simple03.py "Carlos" 15 12 14
---
+++ ruiz/simple03.py
@@ -0,0 +1,23 @@
+# Programa 03
+#Tipo de alumno
+import os
+#Declaracion de variables
+alumno=""
+nota1=0.0
+nota2=0.0
+nota3=0.0
+prom=0.0
+mensaje=""
+#INPUT VIA OS
+alumno=(os.sys.argv[1])
+nota1=float(os.sys.argv[2])
+nota2=float(os.sys.argv[3])
+nota3=float(os.sys.argv[4])
+
+#PROCESSING
+prom=float((nota1+nota2+nota3)/3)
+#Si el promedio del alumno es mayor a 15 sera bueno
+if  (prom >= 14):
+    mensaje="bueno"
+#OUTPUT
+print("Alumno :",prom," ",mensaje)
