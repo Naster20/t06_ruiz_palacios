0abbc32da52ce150ed99464f626752f1d33f5391
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-08 17:37:32 -0500

    Condicional doble 05

---
+++ ruiz/doble05.py
@@ -0,0 +1,22 @@
+# Programa 05
+import os
+#Declaracion de variables
+atleta=""
+peso_antes_de_entrenamiento=0.0
+peso_despues_de_entrenamiento=0.0
+mensaje=""
+#Input via os
+atleta=os.sys.argv[1]
+peso_antes_de_entrenamiento=float(os.sys.argv[2])
+peso_despues_de_entrenamiento=float(os.sys.argv[3])
+#processing
+diferencia_peso=(peso_antes_de_entrenamiento-peso_despues_de_entrenamiento)
+# Si la diferencia de peso es mayor de 7kg entonces fue un excelente entrenamiento
+if(diferencia_peso > 7):
+    mensaje = "Excelente entrenamiento"
+# Si la diferencia de peso es menor de 7kg entonces fue un pesimo entrenamiento
+else:
+    mensaje = "Pesimo Entrenamiento"
+#Output
+print(atleta,"perdiste",diferencia_peso,"Kg",mensaje)
+#fin_if
