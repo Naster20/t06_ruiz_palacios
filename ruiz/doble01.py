9db701f11ccc6cd96933f349b2bec57b1084a4a5
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-08 16:59:38 -0500

    Condicional doble 01

---
+++ ruiz/doble01.py
@@ -0,0 +1,18 @@
+# Programa 01
+import os
+#DECLARACION DE VARIABLES
+usuario=""
+mensaje=""
+#INPUT VIA OS
+usuario=os.sys.argv[1]
+
+#PROCESSING
+#Si el usuario es igual a "Ruiz" mostrar Administrador reconocido
+if(usuario == "Ruiz"):
+    mensaje = "Administrador Reconocido"
+else:
+    mensaje = "Datos Incorrectos"
+#OUTPUT
+print(mensaje)
+
+#fin_if
