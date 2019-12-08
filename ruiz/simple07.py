db323c5995af3be7c23d4c0a8c1204494430e6aa
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-08 01:06:21 -0500

    Condicional simple 07

---
+++ ruiz/simple07.py
@@ -0,0 +1,14 @@
+#Programa 07
+import os
+#Declaracion de variables
+alumno=""
+nota=0.0
+mensaje=""
+#Input via OS
+alumno=(os.sys.argv[1])
+nota=float((os.sys.argv[2]))
+#Processing
+if(nota >= 11):
+    mensaje=("APROBADO")
+
+print(mensaje)
